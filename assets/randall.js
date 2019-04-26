77472b470f771442961d7282fe160e14c674b8e7
Randall <randall@Randalls-MacBook-Pro.local>
2019-04-26 12:07:59 -0700

    a embedded player in the works

--- assets/randall.js
+++ assets/randall.js
@@ -55,5 +55,24 @@ ytSearch = function(video) {
         method: "GET",
     }).then(function(response){
         console.log(response)
+        //when .technique is clicked display title to div
+        var vidResults = $("<div>");
+        console.log(response.items[0]);
+        for ( k = 0; k < response.items.length; k++){
+            var videoTitle = response.items[k].snippet.title;
+            $("<p>").text(JSON.stringify(videoTitle)); 
+        };
+        vidResults.text(videoTitle);
+        // display video to div 
+        var video = response.items;
+        var html = "";
+        video.forEach(function(item){
+            // Include the YouTube Watch URL youtu.be 
+            html += '<p><a href="http://youtu.be/' + item.id + '">';
+            // Add the default video thumbnail (default quality)
+            html += '<img src="http://i.ytimg.com/vi/' + item.id + '/default.jpg">';
+        });
+        //append all
+        $(".technique").append(vidResults, video, html)
     });
 };
