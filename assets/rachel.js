8001948f521fc3d5a6075f8487c1176855e6f9f4
racheldmiller <rachelmiller415@gmail.com>
2019-04-27 13:27:31 -0700

    added more divs

--- assets/rachel.js
+++ assets/rachel.js
@@ -6,17 +6,12 @@ $(document).on("click", "#search", function() {
     .trim();
   console.log(term);
 
-  //   recipeSearch = function(term) {
-  //     console.log(term);
-
   // var appId = config.APP_ID;
   // var appKey = config.APP_KEY;
 
   // Do I need this?
   // "Content-Type: application/json"
 
-  //   var searchTerm = "broccoli";
-  //   console.log("broccoli");
   var queryURL =
     "https://api.edamam.com/search?q=" +
     term +
@@ -33,17 +28,18 @@ $(document).on("click", "#search", function() {
     var results = response.hits;
     var newarray = [];
 
-    for(var p = 0; p < results.length; p++) {
+    for (var p = 0; p < results.length; p++) {
       var recipe = results[p].recipe;
       var newItem = {};
       // add title as attribute
-      newItem.title = recipe.label
+      newItem.title = recipe.label;
       // add image as attribute
-      newItem.img = recipe.image
+      newItem.img = recipe.image;
       // add ingredients as attribute
-      newItem.text = recipe.ingredientLines
+      newItem.text = recipe.ingredientLines;
 
       // CS add html link as an attribute ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      newItem.html = recipe.url;
 
       newarray.push(newItem);
     }
@@ -51,57 +47,61 @@ $(document).on("click", "#search", function() {
 
     cardmaker(newarray);
   });
-
 });
 
 function cardmaker(array, location) {
   // array should have the following elements:
   // each recipe should have:
-    // recipe.title
-    // recipe.img
-    // recipe.text
+  // recipe.title
+  // recipe.img
+  // recipe.text
 
-    // there are 10 responses, so we need to do some sort of loop;
-    for (var i = 0; i < array.length; i++) {
-      // inside the loop, we should be creating a containerdiv
-      console.log(array[1].title);
+  // there are 10 responses, so we need to do some sort of loop;
+  for (var i = 0; i < array.length; i++) {
+    // inside the loop, we should be creating a containerdiv
+    console.log(array[1].title);
 
-      recipe = array[i]
+    var recipe = array[i];
 
-      // CS: new overarchingdiv here
+    $overarchingDiv = $("<div>"); // new div
+    $overarchingDiv.addClass("overarching");
+    $containerDiv = $("<div>");
+    $containerDiv.addClass("container");
 
-      $containerDiv = $("<div>");
+    title = $("<h1>").text(recipe.title);
+    img = $("<img>").attr("src", recipe.img);
+    $ingr = $("<ul>");
 
-      title = $("<h1>").text(recipe.title); // "title, img, ingr" all taken from example in Edamam documentation
+    // .text(recipe.ingredientLines[j]);
 
-      img = $("<img>").attr("src", recipe.img); // found this looking in the console.. might not be right
-      $ingr = $("<ul>");
-      // .text(recipe.ingredientLines[j]);
-
-      for (var j = 0; j < recipe.text.length; j++) {
-        var $list = $("<li>").text(recipe.text[j]);
-        $ingr.append($list);
-      }
-
-      // add classes to each element and the containerdiv
-      title.addClass("title");
-      img.addClass("image");
-      $ingr.addClass("ingredients");
-
-      $containerDiv.append(title);
-      $containerDiv.append(img);
-      $containerDiv.append($ingr);
-
-
-
-      // CS: Create a new div for the youtube link
-      // Add an attribute title with the value recipe.title
-      // Insert text into the div that says something like, search for youtube!
-      // add a class for this div
-
-      // append the $containerdiv and the youtube div above into the overarching div
-
-      // Append overarching div instead of containerdiv
-      $("#bodydiv").append($containerDiv);
+    for (var j = 0; j < recipe.text.length; j++) {
+      var $list = $("<li>").text(recipe.text[j]);
+      $ingr.append($list);
     }
+
+    // add classes to each element and the containerdiv
+    title.addClass("title");
+    img.addClass("image");
+    $ingr.addClass("ingredients");
+
+    $containerDiv.append(title);
+    $containerDiv.append(img);
+    $containerDiv.append($ingr);
+
+    // CS: Create a new div for the youtube link
+    $youtubeDiv = $("<div>");
+
+    // Add an attribute title with the value recipe.title
+    $youtubeDiv.attr("title", recipe.title);
+    // Insert text into the div that says something like, search for youtube!
+    $youtubeDiv.text("search for youtube!");
+    // add a class for this div
+    $youtubeDiv.addClass("youtube");
+
+    // append the $containerdiv and the youtube div above into the overarching div
+    $overarchingDiv.append($containerDiv);
+    $overarchingDiv.append($youtubeDiv);
+    // Append overarching div instead of containerdiv
+    $("#bodydiv").append($overarchingDiv);
   }
+}
